ee36bd8cbe4091b5e1af7c49b15a3103f093e324
mwxgaf <mwxgaf@gmail.com>
2022-09-17 14:55:37 +0430

    Now dynamic rtl in reading mode is complete

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
+// In the name of Allah
+
 import { Plugin } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
 
 	async onload() {
 		this.registerMarkdownPostProcessor((element, context) => {
-			element.querySelectorAll('p').forEach(e => {
+			element.querySelectorAll('p,div.cm-line,div.callout-title-inner,h1,h2,h3,h4,h5,h6').forEach(e => {
 				e.setAttribute('dir','auto');
+			});
+			element.querySelectorAll('table,ol,ul,pre').forEach(e => {
+				e.parentElement?.setAttribute('dir','auto');
 			});
 		});
 	}
