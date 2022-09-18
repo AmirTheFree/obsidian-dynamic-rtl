9f93d59365a7c1e444d5fa20c2a03380faee2839
mwxgaf <mwxgaf@gmail.com>
2022-09-18 14:37:18 +0430

    Cleaned main.ts a little

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 // In the name of Allah
 
-import { Plugin } from 'obsidian';
+import { Plugin, MarkdownView, Editor } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
 
 	async onload() {
 
 		// Reading view (for tables & callouts this fixes the editor too)
-		this.registerMarkdownPostProcessor((element, context) => {
-			element.querySelectorAll('p,div.cm-line,div.callout-title-inner,h1,h2,h3,h4,h5,h6').forEach(e => {
-				e.setAttribute('dir', 'auto');
+		this.registerMarkdownPostProcessor((container, context) => {
+			container.querySelectorAll('p,div.cm-line,div.callout-title-inner,h1,h2,h3,h4,h5,h6').forEach(element => {
+				element.setAttribute('dir', 'auto');
 			});
-			element.querySelectorAll('table,ol,ul,pre').forEach(e => {
-				e.parentElement?.setAttribute('dir', 'auto');
+			container.querySelectorAll('table,ol,ul,pre').forEach(element => {
+				element.parentElement?.setAttribute('dir', 'auto');
 			});
 		});
 	}
