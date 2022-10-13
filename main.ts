5da7f7114f3b3149866bdc6f270128824b89f877
mwxgaf <mwxgaf@gmail.com>
2022-10-13 23:24:28 +0330

    Fixed conflics with Obsidian v1.0.0 | Ready for v0.2.0

--- main.ts
+++ main.ts
@@ -6,13 +6,33 @@ export default class DynamicRTL extends Plugin {
 
 	async onload() {
 
-		// Reading view (for tables & callouts this fixes the editor too)
+		const chars = ['ا','ب','پ','ت','ث','ج','چ','ح','خ','س','ش','د','ذ','ر','ز','ژ','س','ش','ص','ض','ط','ظ','ع','غ','ک','گ','ل','م','ن','و','ه','ی','۱','۲','۳','۴','۵','۶','۷','۸','۹','ي','ئ','آ','ك','ء','ؤ','إ','أ','ة'];
+
 		this.registerMarkdownPostProcessor((container, context) => {
-			container.querySelectorAll('p,div.cm-line,div.callout-title-inner,h1,h2,h3,h4,h5,h6').forEach(element => {
+			// Fixes the Reading view (for tables & callouts this fixes the editor too)
+			container.querySelectorAll('p,div.cm-line,h1,h2,h3,h4,h5,h6').forEach(element => {
 				element.setAttribute('dir', 'auto');
 			});
 			container.querySelectorAll('table,ol,ul,pre').forEach(element => {
 				element.parentElement?.setAttribute('dir', 'auto');
+			});
+			// Fixes the qoutes border direction in RTL texts
+			container.querySelectorAll('blockquote').forEach(element => {
+				if (chars.includes(element.innerText.charAt(1))) {
+					element.style.borderLeft = '0';
+					element.style.borderRight = 'var(--blockquote-border-thickness) solid var(--blockquote-border-color)';
+					element.style.marginRight = '23px';
+					element.querySelector('p').style.marginRight = '23px';
+				}
+			});
+			// Fixed the bullet points problem in reading mode
+			container.querySelectorAll('ul').forEach(element => {
+				if (chars.includes(element.innerText.charAt(1))) {
+					element.querySelectorAll('.list-bullet').forEach(bullet => {
+						bullet.style.float = 'right';
+						bullet.classList.add('rtl-bullet-point');
+					});
+				}
 			});
 		});
 
